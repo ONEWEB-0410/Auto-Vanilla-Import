6eb5e483a53a94ff026806f86dec180167ed55f2
ONEWEB-0410 <69854495+ONEWEB-0410@users.noreply.github.com>
2022-08-15 19:21:31 +0300

    Update autoimport.bat

--- autoimport.bat
+++ autoimport.bat
@@ -10,6 +10,8 @@ if exist %_a%%_b%%_a%\AppData\Local\Roblox\Versions\%a% (echo "Loading...") else
 
 if exist %0\..\ClassImages.png (echo "Copying files") else (echo "There is no file with the name ClassImages.png. Please make sure that you have put it in the same folder with the .bat program")
 
+del %_a%%_b%%_a%\AppData\Local\Roblox\Versions\%a%\content\textures\ClassImages.png
+
 copy "%0\..\ClassImages.png"  "%_a%%_b%%_a%\AppData\Local\Roblox\Versions\%a%\content\textures"
 
 
